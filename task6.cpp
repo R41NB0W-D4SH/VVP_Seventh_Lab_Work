1521835cae113b099a1ce507efa0650b7641d42c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-21 13:13:57 +0300

    Create task6.cpp

---
+++ task6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c, d, e;
+	cout << "Введите стороны треугольника ABC. (Сначала A, потом B, потом C) " << endl;
+	cin >> a >> b >> c;
+
+	if (pow(a, 2) + pow(b, 2) == pow(c, 2))
+	{
+		cout << "Треугольник ABC является прямоугольным!" << endl;
+	}
+	else
+	{
+		cout << "Треугольник ABC НЕ является прямоугольным!" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
